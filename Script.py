b5a50ec259015c4425eede676dc64ebcc47de43e
sujay5372bot <sujay53721@gmail.com>
2026-09-24 09:13:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -269,7 +269,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/onefighterarmy">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/onefighterarmy">[ SUJAY 😎 ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
